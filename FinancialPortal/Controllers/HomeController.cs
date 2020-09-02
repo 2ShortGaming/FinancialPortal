6c4e7a19d7b903750534b3b829f965f54723f304
2ShortGaming <66035017+2ShortGaming@users.noreply.github.com>
2020-09-02 11:17:56 -0400

    Initial commit

---
+++ FinancialPortal/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using FinancialPortal.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FinancialPortal.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public PartialViewResult _LoginPartial()
+        {
+            var userId = User.Identity.GetUserId();
+            var user = db.Users.Find(userId);
+            return PartialView(user);
+        }
+    }
+}
